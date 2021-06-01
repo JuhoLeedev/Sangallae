d1919c26bcf2c8a7250464f8eb5a34c653f41015
juho <juholeedev@daum.net>
2021-06-01 22:36:29 +0900

    map inserted

--- app/src/main/java/com/example/sangallae/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/sangallae/ui/map/MapFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sangallae.R
+
 
 class MapFragment : Fragment() {
 
@@ -17,8 +16,8 @@ class MapFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
-        val textView: TextView = root.findViewById(R.id.text_map)
-        mapViewModel.text.observe(viewLifecycleOwner, Observer { textView.text = it })
+
+
         return root
     }
 }
