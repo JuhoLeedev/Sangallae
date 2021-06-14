40054ea8ef03eba167ca3b52e48086464e2d966d
juho <juholeedev@daum.net>
2021-06-15 04:33:36 +0900

    debugging_3

--- app/src/main/java/com/example/sangallae/utils/Info.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sangallae.utils
-
-object Info {
-    const val AWS_ACCESS_KEY: String = "AKIAVHYMFOL7ZLMW5ZN7"
-    const val AWS_SECRET_KEY: String = "KW2yBRt/qCARTciyo5w2EpMH/g5x0006nN8DX6g3"
-}
