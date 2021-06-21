7a7bc6cfbfe8e1dbd135be8d517c7d90d61c69fb
juho <juholeedev@daum.net>
2021-06-21 13:27:49 +0900

    permission

---
+++ app/src/main/java/com/example/sangallae/utils/Info.kt
@@ -0,0 +1,6 @@
+package com.example.sangallae.utils
+
+object Info {
+    const val AWS_ACCESS_KEY: String = "AKIAVHYMFOL77CFFX27J"
+    const val AWS_SECRET_KEY: String = "7akbS33OPBb0D6rHku2f/Wa0wOeOUBD6WfMf7SgL"
+}
