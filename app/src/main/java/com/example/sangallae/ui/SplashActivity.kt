edbe7184401be03c8e64357ab6d4169dced0d5ef
juho <juholeedev@daum.net>
2021-06-21 14:19:44 +0900

    permission

--- app/src/main/java/com/example/sangallae/ui/SplashActivity.kt
+++ app/src/main/java/com/example/sangallae/ui/SplashActivity.kt
@@ -30,26 +30,30 @@ class SplashActivity : AppCompatActivity() {
         val mContext = this
 
         //구글 자동로그인
-        if(firebaseAuth?.currentUser != null){
-            Log.d("start", "google: "+firebaseAuth?.currentUser.toString())
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-        //네이버 자동로그인
-        else if(mOAuthLoginInstance.getAccessToken(mContext)!=null){
-            Log.d("start", "naver: "+mOAuthLoginInstance.getAccessToken(mContext).toString())
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-        //카카오 자동로그인
-        else if(TokenManager.instance.getToken()!= null){
-            Log.d("start", "kakao: "+ OAuthToken.toString())
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-        else { //로그인 정보 없음
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }
+//        if(firebaseAuth.currentUser != null){
+//            Log.d("start", "google: "+ firebaseAuth.currentUser.toString())
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }
+//        //네이버 자동로그인
+//        else if(mOAuthLoginInstance.getAccessToken(mContext)!=null){
+//            Log.d("start", "naver: "+mOAuthLoginInstance.getAccessToken(mContext).toString())
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }
+//        //카카오 자동로그인
+//        else if(TokenManager.instance.getToken()!= null){
+//            Log.d("start", "kakao: "+ OAuthToken.toString())
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }
+//        else { //로그인 정보 없음
+//            startActivity(Intent(this, LoginActivity::class.java))
+//            finish()
+//        }
+
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
